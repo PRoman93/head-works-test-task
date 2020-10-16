c6867a5bff1f3c1cbe3f624eff480188927eb9d4
PRoman93 <roman_prokopets@mail.ua>
2020-10-16 11:25:54 +0300

    Updates

---
+++ precache-manifest.a0ffb54205a639088589d1b878b3deae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e7947198149f812ac9eb26ad88eee0d",
+    "url": "/head-works-test-task/index.html"
+  },
+  {
+    "revision": "dbd7207568e2c37744b4",
+    "url": "/head-works-test-task/static/css/main.955b5e12.chunk.css"
+  },
+  {
+    "revision": "9a8428d27a85f2369712",
+    "url": "/head-works-test-task/static/js/2.cb9c2fca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/head-works-test-task/static/js/2.cb9c2fca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbd7207568e2c37744b4",
+    "url": "/head-works-test-task/static/js/main.a5b3007b.chunk.js"
+  },
+  {
+    "revision": "21996c7782ba9bc5463b",
+    "url": "/head-works-test-task/static/js/runtime-main.b7d31b6e.js"
+  },
+  {
+    "revision": "0a80076b093682cc02d067177068bcd8",
+    "url": "/head-works-test-task/static/media/preloader.0a80076b.gif"
+  }
+]);
